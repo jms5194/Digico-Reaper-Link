4baf0b4c0e9a80f6b5e61beb7ab603a3b6b3e21d
jms5194 <justinstasiw@gmail.com>
2025-07-27 09:45:54 -0700

    Pro tools updates

--- daws/protools.py
+++ daws/protools.py
@@ -7,6 +7,7 @@ from logger_config import logger
 import threading
 import time
 import sys
+from typing import Optional
 
 class ProTools(Daw):
     type = "ProTools"
@@ -32,11 +33,49 @@ class ProTools(Daw):
         self.pt_engine_connection = ptsl.client.Client(company_name="JSSD",
                                          application_name=sys.argv[0])
         if self.pt_engine_connection is not None:
-            if self.pt_engine_connection.host_ready_check():
-                print("connected")
+            logger.info("Connection established to Pro Tools")
+
+    def run_command_on_session(self, command_id: pt.CommandId,
+                               args: dict) -> Optional[dict]:
+        if self.pt_engine_connection is None:
+            logger.error("Command failed, not connected to Pro Tools")
+            return None
+
+        try:
+            r = self.pt_engine_connection.run_command(command_id, args)
+            return r
+        except ptsl.errors.CommandError as e:
+            if e.error_type == pt.PT_NoOpenedSession:
+                logger.error("Command failed, no session is currently open in Pro Tools")
+                return None
+        except Exception:
+            logger.error("Command failed, Pro Tools may not be running")
+            return None
+
+    def do_newmemloc(self, args):
+        'Create a new marker memory location: NEWMEMLOC start-time'
+        command_args = {'name': 'New Marker',
+                        'start_time': args.strip(),
+                        'end_time': args.strip(),
+                        'time_properties': 'TP_Marker',
+                        'reference': 'MLR_FollowTrackTimebase',
+                        'general_properties': {
+                            'zoom_settings': False,
+                            'pre_post_roll_times': False,
+                            'track_visibility': False,
+                            'track_heights': False,
+                            'group_enables': False,
+                            'window_configuration': False,
+                        },
+                        'comments': "Created by toolshell",
+                        'color_index': 1,
+                        'location': 'MLC_MainRuler'
+                        }
+
+        self.run_command_on_session(pt.CreateMemoryLocation, command_args)
 
     def _place_marker_with_name(self, marker_name):
-        pass
+        self.do_newmemloc("00:01:00:00")
 
     def _incoming_transport_action(self, transport_action):
         pass
